4c5d826298f4f7764ed8be8f0e388e33a116391e
macjohnalds <john_nguy11@hotmail.com>
2020-03-20 16:36:38 -0700

    implemented flask sql-alchemy

--- albums.py
+++ albums.py
@@ -1,85 +1,120 @@
-class User:
-    def __init__(self):
-        self.__first_name = ""
-        self.__last_name = ""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-    @property
-    def first_name(self):
-        return self.__first_name
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///applemusic.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    @property
-    def last_name(self):
-        return self.__last_name
-    
-    @first_name.setter
-    def first_name(self, value):
-        self.__first_name = value
 
-    @last_name.setter
-    def last_name(self, value):
-        self.__last_name = value
+class User(db.Model):
+    __tablename__ = "users"
 
-    def make_playlist(self, name):
-        self.private_playlist = PrivatePlaylist(name)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return (f"Name: {self.username}\n Email: {self.email}\n Password: {self.password}")
+
+    def __hash__(self):
+        return hash(self.username)
+
+    def make_playlist(self, username):
+        self.private_playlist = PrivatePlaylist(username)
 
     def remove_playlist(self):
         del self.private_playlist
 
 
-class Album:
-    def __init__(self, name, year, cover_img):
-        self.album_name_holder = name
-        self.name = name
-        self.year = year
-        self.cover_img = cover_img
-        self.song_list = []
+class Album(db.Model):
+    #def __init__(self, name, year, cover_img):
+    __tablename__ = "albums"
+    
+    #name = db.Column(db.String(50), )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    #cover_img = db.Column(db.BLOB, nullable=True, default="default.jpg")
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=True)
+    artist = db.relationship("Artist", backref="albums",lazy=True)
 
-    def add_song(self, name, duration, lyrics, artist_name, language):
-        # Compositional instantiation
-        self.song = Song(name, duration, lyrics, artist_name, language)
-        self.song_list.append(self.song)
+
+    # def add_song(self, name, duration, lyrics, artist_name):
+    #     # Compositional instantiation
+    #     self.song = Song(name, duration, lyrics, artist_name)
+    #     self.song_list.append(self.song)
         # If the song length = 1, the album will be the name of the song. Else, name will be the instantiated name
-        if len(self.song_list) == 1:
-            self.name = self.song.name
-            self.song.image = None
-        else: 
-            self.name = self.album_name_holder
-            self.song.image = self.cover_img
+        # if len(self.song_list) == 1:
+        #     self.name = self.song.name
+        #     self.song.image = None
+        # else: 
+        #     self.name = self.album_name_holder
+        #     self.song.image = self.cover_img
 
-    def remove_song(self, remove):
-        remove = remove - 1
-        self.song_list.pop(remove)
+    # def remove_song(self, remove):
+    #     remove = remove - 1
+    #     self.song_list.pop(remove)
 
-        if len(self.song_list) == 1:
-            self.name = self.song.name
-            self.song.image = None
-        else: 
-            self.name = self.album_name_holder
-            self.song.image = self.cover_img
+    #     if len(self.song_list) == 1:
+    #         self.name = self.song.name
+    #         self.song.image = None
+    #     else: 
+    #         self.name = self.album_name_holder
+    #         self.song.image = self.cover_img
 
 
-class Song:
-    def __init__(self, name, duration, lyrics, artist_name, language, image=None):
-        self.name = name
-        self.duration = duration
-        self.lyrics = lyrics
-        self.artist = Artist(artist_name, language)
-        self.image = image
+# class Single(db.Model):
+#     #def __init__(self, name, year, cover_img):
+#     __tablename__ = "singles"
+    
+#     # name = db.Column(db.String(50), )
+#     id = db.Column(db.Integer, primary_key=True)
+#     # name = db.Column(db.String(50), unique=False, nullable=False)
+#     # name_id = db.relationship("Song", primaryjoin="and_(Single.id==Song.single_id)")
+#     year_id = db.Column(db.Integer, db.ForeignKey("songs.id"), nullable=False)
+#     year = db.relationship("Song",backref="singles_year", lazy=True)
+#     #cover_img = db.Column(db.BLOB, nullable=True, default="default.jpg")
+#     artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=True)
+#     artist = db.relationship("Artist", backref="singles_artist",lazy=True)
 
-    def __repr__(self):
+
+class Song(db.Model):
+    __tablename__ = "songs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), unique=False, nullable=False)
+    duration = db.Column(db.Integer, unique=False, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    lyrics = db.Column(db.String(255), unique=False, nullable=False)
+    # image = db.Column(db.BLOB, nullable=False, default="default.jpg")
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
+    artist = db.relationship("Artist", backref="songs",lazy=True)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=True)
+    album = db.relationship("Album", backref="songs",lazy=True)
+    # single_id = db.Column(db.Integer, db.ForeignKey("singles.id"), nullable=True)
+    # single = db.relationship("Single", backref="songs",lazy=True)
+
+
+    def __rep__(self):
         return(f"Song Name: {self.name}\nSong Length: {self.duration}\nLyrics: {self.lyrics}\nArtist: {self.artist.name}\nLanguage: {self.artist.language}\nAlbum Cover: {self.image}")
 
 
-class Artist: 
-    def __init__(self, name, language="English"):
-        self.name = name
-        self.language = language
+class Artist(db.Model): 
+    __tablename__ = "artists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), unique=False, nullable=False)
 
 
-class PrivatePlaylist: 
-    def __init__(self, name):
-        self.name = name
-        self.songs = []
+class PrivatePlaylist(db.Model): 
+    __tablename__ = "private_playlist"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    username_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    username = db.relationship("User", backref="privateplaylist", lazy=True)
+    # def __init__(self, name):
+    #     self.name = name
+    #     self.songs = []
     
     def add_song(self, song):
         self.song = song
@@ -94,36 +129,111 @@ class PrivatePlaylist:
         for song in self.songs:
             # song.song refers to each song inside the self.songs list and referring to self.song inside the Album class (line 39)
             print(song.song, "\n")
-    
+
+
+
+db.create_all()
+
+john = User(username="John", email="john@hotmail.com", password="1234")
+db.session.add(john)
+
+
+singer = Artist(name="Jeff")
+db.session.add(singer)
+
 
 # Instantiate Album and add song.
-first_album = Album("Jeff's Album", 2020, "Jeff's Album Cover")
-first_album.add_song("Jeff's song", "300s", "These are my lyrics", "Jeff", "English")
+first_album = Album(name="Jeff", year=2020, artist=singer)
+print(first_album.artist.name)
+#first_album.add_song("Jeff's song", "300s", "These are my lyrics", "Jeff", "English")
+db.session.add(first_album)
+
+song = Song(name="Happy", duration=300, year=2020, lyrics='Happyyyier', artist=singer, album=first_album)
+db.session.add(song)
+
+# Instantiate Album and add song.
+
+# first_single = Single(name=[song], artist=singer)
+# print(first_single.name)
+# db.session.add(first_single)
+
+# song = Song(name="Happy", duration=300, year=2020, lyrics='Happyyyier', artist=singer, album=first_album)
+# db.session.add(song)
+
+# song = Song(name="Happy", duration=300, year=2020, lyrics='Happyyyier', artist= singer, album=first_album)
+# db.session.add(song)
+
+# Instantiate Private Playlist
+playlist_name = PrivatePlaylist(name="Emmy's Playlist", username=john)
+print(playlist_name.username.username)
+#playlist_name.add_song("Emmy's Song")
+db.session.add(playlist_name)
+
+db.session.commit()
 
 
-# Instantiate User and make a private playlist (compositional relationship) using make_playlist method. 
-john = User()
-john.first_name = "John"
-john.last_name = "Nguy"
 
-# We instantiate PrivatePlaylist class using method belonging to User class called make_playlist (Line 22).
-john.make_playlist("John's playlist")
 
-# Now instance of User has access to instance of PrivatePlaylist methods. 
-# PrivatePlaylist class is instantiated as self.private_playlist inside the User class (See line 23).
 
-# We call the add_song method that we now have access to, adding our "single" first_album. 
-john.private_playlist.add_song(first_album)
 
-# this is calling the method inside PrivatePlaylist class to show all songs inside that playlist
-john.private_playlist.show_songs_in_playlist()
 
-# Shows your PrivatePlaylist instance with name "John's playlist"
-print(john.private_playlist.name)
 
-# Removes a users playlist!
-john.remove_playlist()
-print(john.private_playlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app.py
+++ app.py
@@ -1,43 +1,51 @@
-class Album:
-    def __init__(self, name, year, songs, cover_img, language, artist=None, song_list=None):
-        self.name = name
-        self.year = year
-        self.songs = songs
-        self.cover_img = cover_img
-        self.language = language
-        if artist is None:
-            self.artist = Artist("Album Artists")
-        else:
-            self.artist = artist
-        self.song_list = []
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+import sqlite3
+from sqlite3 import Error
+from flask import g
+import os.path
 
-    def add_song(self, song, position=None):
-        if position is None:
-            self.song_list.append(song)
-        else:
-            self.song_list.insert(position, song)
+# DATABASE = 'path'
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
+
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        cur = conn.cursor()
+        who = "Yeltsin"
+        age = 72
+        cur.execute("create table people (name_last, age)")
+        cur.execute("insert into people values (?, ?)", (who, age))
+        print('its in database')
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     message = 'this is a song'
+#     return render_template('index.html', message=message)
 
 
-class Song:
-    def __init__(self, name, duration, lyrics, artist_name, language, album_name, cover_img, year, song):
-        self.name = name
-        self.duration = duration
-        self.lyrics = lyrics
-        self.artist = Artist(artist_name, language)
-        self.image = Album(album_name, cover_img, year, song)
 
-
-class Artist:
-    def __init__(self, name, language="English", albums=1):
-        self.name = name
-        self.language = language
-        self.albums = []
-
-    def add_album(self, album):
-        self.albums.append(album)
-
-
-class Playlist:
-    def __init__(self, name, songs=0):
-        self.name = name
-        self.songs = []
+if __name__ == '__main__':
+    create_connection("/Users/macjohnalds/desktop/oop/project/n.w.a/music.db")
+    app.run()0
